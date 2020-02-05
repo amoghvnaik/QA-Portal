d47aa1e8f0a2df250da6173c40bc97a6a5d636cc
amoghvnaik <57941369+amoghvnaik@users.noreply.github.com>
2020-02-05 14:41:54 +0000

    Update app-init.ts

--- qa-portal-angular/app-init.ts
+++ qa-portal-angular/app-init.ts
@@ -5,7 +5,7 @@ export function initializer(keycloak: KeycloakService): () => Promise<any> {
 
   const options: KeycloakOptions = {
     config: {
-      url: 'http://35.195.203.87/auth',
+      url: 'http://35.205.152.8/auth',
       realm: 'qa-portal',
       clientId: 'qa-portal-ui'
     },
